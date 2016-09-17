abb49d46e4c5a63dd9e87d761633612de82645ba
Waihon Yew <yewwaihon@gmail.com>
2016-09-17 20:50:15 +0800

    Complete select

---
+++ select.rb
@@ -0,0 +1,10 @@
+# select { |obj| block } → array
+# select → an_enumerator
+#
+# Returns an array containing all elements of enum for which the given block 
+# returns a true value.
+#
+# If no block is given, an Enumerator is returned instead.
+#
+# select and find_all are two names for the same method.
+
